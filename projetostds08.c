e7ea4f2f0764117ef6549fdea37653f1dd11e915
Kleber Petry <kleberpetry@gmail.com>
2022-04-18 14:35:08 -0300

    Update projetostds08.c

--- projetostds08.c
+++ projetostds08.c
@@ -1,15 +1,55 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<locale.h>
+#include<stdbool.h>
 struct projeto{
-	
+	int codido;
+	char titulo[50];
+	char descricao[50];
+	int ano;
+	int status;
+	char resposavel[50];
 };
 struct projeto projetos[200];
+const int a=1,f=2,c=3;
+int sequencia=0;
 
 cadastrarProjeto(){
-	//identico ao da triagem;
+	char continuar;
+	bool cadastro=true;
+	do{
+		system("cls");
+		projetos[sequencia].codido=sequencia+1;
+		printf("\nDigite o titulo do projeto: ");
+		fflush(stdin);
+		gets(projetos[sequencia].titulo);
+		printf("\nDigite a descrição do projeto: ");
+		fflush(stdin);
+		gets(projetos[sequencia].descricao);
+		printf("\nDigite o ano do projeto: ");
+		scanf("%d",&projetos[sequencia].ano);
+		printf("\nDigite o status do projeto: ");
+		scanf("%d",&projetos[sequencia].status);
+		printf("\nDigite o responsável: ");
+		fflush(stdin);
+		gets(projetos[sequencia].resposavel);
+		printf("\nProjeto cadastrado com sucesso! código gerado %d",projetos[sequencia].codido);
+		printf("\nDeseja cadastrar um novo projeto? S-sim N-não ");
+		fflush(stdin);
+		scanf("%c",&continuar);
+		if(continuar=='n'||continuar=='N'){
+			cadastro=false;
+		}
+		sequencia++;
+		
+	}while(cadastro!=false);
+	
 }
-//todosProjetos(){}
+todosProjetos(){
+	
+
+
+}
 //porStatus(){}
 
 main(){
@@ -23,7 +63,7 @@ main(){
 		scanf("%d",&op);
 		switch(op){
 			case 1:
-				//cadastrarProjeto();
+				cadastrarProjeto();
 			break;
 			case 2:
 				//todosProjetos();
