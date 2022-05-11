1a718cc35fa2d69142b9a64687903327b9c34b59
Kleber Petry <kleberpetry@gmail.com>
2022-05-11 14:59:43 -0300

    alterações

--- calculadora classe/main.cpp
+++ calculadora classe/main.cpp
@@ -6,13 +6,18 @@ main(){
 	setlocale(LC_ALL, "Portuguese");
 	double num1,num2,resultado;
 	int operacao;
-	printf("\nDigite o primeiro numero: ");
-	scanf("%lf",&num1);
-	printf("\nDigite o segundo numero: ");
-	scanf("%lf",&num2);
-	printf("\nDigite a operação\n1-soma\n2-subtração\n3-divisão\n4-multiplicação\n");
-	scanf("%d",&operacao);
-	resultado=calculadora(num1,num2,operacao);
-	printf("\nO resultado é: %0.2lf\n",resultado);
+	do{
+		system("cls");
+		resultado=0;
+		printf("\nDigite o primeiro numero: ");
+		scanf("%lf",&num1);
+		printf("\nDigite o segundo numero: ");
+		scanf("%lf",&num2);
+		printf("\nDigite a operação\n1-soma\n2-subtração\n3-divisão\n4-multiplicação\n");
+		scanf("%d",&operacao);
+		resultado=calculadora(num1,num2,operacao);
+		printf("\nO resultado é: %0.2lf\n",resultado);
+		system("pause");
+	}while(operacao!=0);
 	return 0;
 }
